7318b0a3efbddf686724e4f88cfc27cfb350c193
Darrow Cole <darrowcoucla_03@darrow.com>
2021-05-26 09:24:10 -0700

    Apps-815 - Timeout sinai ID logins after three days (#30)
    * initial commit, set cookie to 1.mins for testing
    
    * added second 1-min for testing
    
    * test failed, removed
    
    * changed syntax for 1-min test
    
    * switched from minutes to seconds
    
    * switched to years, seconds problematic?
    
    * restored original
    
    * try year reduction again
    
    * seconds, second try
    
    * verifying cookie rename breaks images
    
    * set 3-day timeout
    
    * fixed typo
    
    * clarified readme error
    
    * added test values
    
    * updated test values
    
    * updated for sinaimanuscripts-dev after server set their server
    
    * changed test image
    
    * cookie domain setting test
    
    * removed cookie domain test
    
    * added test image
    
    * reset expiry to 25 years
    
    * reset expiry to 25 years
    
    * reset expiry to 25 years
    
    * restored cookie name
    
    * updated to 60 sec timeout
    
    * re-updated erb files to _3day cookie
    
    * login params updated, now final testing of cookie obviation
    
    * cookie obviation test
    
    * verifying 3.days syntax
    
    * removed test image
    
    Co-authored-by: darrowcoucla <darrowco@library.ucla.edu>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,9 +37,9 @@ class ApplicationController < ActionController::Base
   end
 
   def sinai_authn_check
-    return true if !Flipflop.sinai? || [login_path, version_path].include?(request.path) || sinai_authenticated?
+    return true if !Flipflop.sinai? || [login_path, version_path].include?(request.path) || sinai_authenticated_3day?
     if ENV['SINAI_ID_BYPASS'] # skip auth in development
-      cookies[:sinai_authenticated] = 'true'
+      cookies[:sinai_authenticated_3day] = 'true'
       return true
     end
     check_document_paths
@@ -65,8 +65,8 @@ class ApplicationController < ActionController::Base
     params[:sort] ||= 'score desc' unless params[:q].to_s.empty?
   end
 
-  def sinai_authenticated?
-    cookies[:sinai_authenticated]
+  def sinai_authenticated_3day?
+    cookies[:sinai_authenticated_3day]
   end
 
   def ucla_token?
@@ -83,14 +83,14 @@ class ApplicationController < ActionController::Base
   end
 
   def set_auth_cookies
-    cookies[:sinai_authenticated] = {
+    cookies[:sinai_authenticated_3day] = {
       value: create_encrypted_string.unpack('H*')[0].upcase,
-      expires: Time.zone.now + 25.years,
+      expires: Time.zone.now + 3.days,
       domain: ENV['DOMAIN']
     }
     cookies[:initialization_vector] = {
       value: cipher_iv.unpack('H*')[0].upcase,
-      expires: Time.zone.now + 25.years,
+      expires: Time.zone.now + 3.days,
       domain: ENV['DOMAIN']
     }
   end
