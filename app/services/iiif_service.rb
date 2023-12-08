52cc3b6c0563b03a603cea0c9548295caf975645
Andy Wallace <andrewwallace@library.ucla.edu>
2023-12-08 16:54:32 -0500

    use DL Viewer

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -2,14 +2,11 @@
 
 class IiifService
   def src(request, document)
-    "#{request&.base_url}/mirador3.html?manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    # "http://localhost:8080/?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
   end
 
   def iiif_manifest_url(document)
-    if Flipflop.use_manifest_store? && document[:iiif_manifest_url_ssi]
-      document[:iiif_manifest_url_ssi].sub('http:', 'https:')
-    else
-      "#{Rails.application.config.iiif_url}/#{document.id}/manifest"
-    end
+    document[:iiif_manifest_url_ssi].sub('http:', 'https:')
   end
 end
