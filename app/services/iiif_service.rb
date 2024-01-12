3aecb4ed6849c36c2320d72802bf8857fd3f04c8
pghorpade <pghorpade@library.ucla.edu>
2024-01-12 11:22:19 -0800

    fix: Add media_viewer_url like ursus

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -2,12 +2,23 @@
 
 class IiifService
   def src(request, document)
-    "#{request&.base_url}?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    "#{media_viewer_url(request)}?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "http://localhost:8080/?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
   end
 
   def iiif_manifest_url(document)
     document[:iiif_manifest_url_ssi].sub('http:', 'https:')
+  end
+
+  def media_viewer_url(request)
+    case request.query_parameters['media_viewer_url']
+    when nil
+      Rails.application.config.media_viewer_url
+    when 'dev', 'test', 'stage', 'prod'
+      'https://' + request.query_parameters['viewer'][0] + '-w-dl-viewer01.library.ucla.edu'
+    else
+      request.query_parameters['viewer']
+    end
   end
 end
