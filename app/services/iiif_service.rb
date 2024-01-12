dbbe8eddb93094857802a6fe3160e26a466bd758
pghorpade <pghorpade@library.ucla.edu>
2024-01-12 10:39:36 -0800

    fix: replace / with ? and remove tinu from codeowners

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -2,7 +2,7 @@
 
 class IiifService
   def src(request, document)
-    "#{request&.base_url}/site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    "#{request&.base_url}?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "http://localhost:8080/?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
   end
