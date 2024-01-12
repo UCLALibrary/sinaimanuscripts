c84be35292897d2fc00efa3c902863afd200f7f0
pghorpade <pghorpade@library.ucla.edu>
2024-01-11 17:31:38 -0800

    fix: replace dlviewer dev with production dl viewer

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -2,8 +2,9 @@
 
 class IiifService
   def src(request, document)
+    "#{request&.base_url}/site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "http://localhost:8080/?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
-    "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    # "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
   end
 
   def iiif_manifest_url(document)
