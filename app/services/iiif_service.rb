9e5959856527a97ee718ac3aba4c69676b7f85ce
Andy Wallace <andrewwallace@library.ucla.edu>
2025-11-06 06:40:59 -0800

    mirador4 feature flag
    Mirador 4 isn't quite ready for prime time, so it was added to dl-viewer as a non-default option. This flag allows us to enable that option on stage.

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -2,7 +2,7 @@
 
 class IiifService
   def src(request, document)
-    "#{media_viewer_url(request)}?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+    "#{media_viewer_url(request)}?#{viewer}&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "http://localhost:8080/?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
     # "https://d-w-dl-viewer01.library.ucla.edu?site=sinai&manifest=#{CGI.escape(iiif_manifest_url(document))}"
   end
@@ -19,6 +19,15 @@ class IiifService
       'https://' + request.query_parameters['viewer'][0] + '-w-dl-viewer01.library.ucla.edu'
     else
       request.query_parameters['viewer']
+    end
+  end
+
+  def viewer
+    if Flipflop.enabled?(:mirador4)
+      'viewer=mirador4'
+    else
+      # use 'site' instead of 'viewer' so dl-viewer can choose different mirador for palimpsests
+      'site=sinai'
     end
   end
 end
